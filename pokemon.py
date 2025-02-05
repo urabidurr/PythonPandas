eddbd8d425546497e9ffc2bde5256b9aad648f61
Marco Quintero <mquintero50@stuy.edu>
2025-02-05 11:47:35 -0500

    End of class

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,10 @@
 import pandas as pd
 pokemon = pd.read_csv("static/d1.csv")
 
-print(pokemon)
+def check_duplicates():
+    pokemon.duplicated()
+
+def remove_duplicates():
+    pokemon.drop_duplicates(inplace = True)
+remove_duplicates()
+print(pokemon.to_string())
